c5e4225b538f650bb87b02f0c776e8c5fa875d19
peptoglide <tuantin09@gmail.com>
2025-03-26 15:28:14 +0700

    Added saving
    To build towers

--- python/src/examplefuncsplayer/bot.py
+++ python/src/examplefuncsplayer/bot.py
@@ -1,11 +1,13 @@
 import random
+from enum import Enum
 #Hello world guys lol
 
 from battlecode25.stubs import *
 
 # This is an example bot written by the developers!
 # Use this to help write your own code, or run it against your bot to see how well you can do!
-
+class MessageType(Enum):
+    SAVE_CHIPS = 0
 
 # Globals
 turn_count = 0
@@ -22,8 +24,8 @@ directions = [
 
 # Should add up to 100
 
-bot_chance = {UnitType.SOLDIER : 33, UnitType.MOPPER : 33, UnitType.SPLASHER : 34}
-tower_chance = {UnitType.LEVEL_ONE_MONEY_TOWER : 45, UnitType.LEVEL_ONE_PAINT_TOWER : 45, UnitType.LEVEL_ONE_DEFENSE_TOWER : 10}
+bot_chance = {UnitType.SOLDIER : 40, UnitType.MOPPER : 25, UnitType.SPLASHER : 35}
+tower_chance = {UnitType.LEVEL_ONE_MONEY_TOWER : 65, UnitType.LEVEL_ONE_PAINT_TOWER : 25, UnitType.LEVEL_ONE_DEFENSE_TOWER : 10}
 bot_name = {UnitType.SOLDIER : "SOLDIER", UnitType.MOPPER : "MOPPER", UnitType.SPLASHER : "SPLASHER"}
 
 def update_bot_chance(soldier, mopper, splasher):
@@ -42,12 +44,20 @@ def get_random_unit(probabilities):
         if n <= prob: return unit
         n -= prob
 
+# Statics
 # Determine build delays between each bot spawned by a tower
 buildDelay = 15 # Tune
 buildDeviation = 3
-buildCooldown = 0
-
 tower_upgrade_threshold = 1
+# When we're trying to build, how long should we save
+save_turns = 70 # Tune
+
+# Privates
+buildCooldown = 0
+is_messanger = False # We designate half of moppers to be messangers
+known_towers = []
+should_save = False
+savingTurns = 0
 
 def turn():
     """
@@ -55,11 +65,13 @@ def turn():
     This function will be called at the beginning of every turn and should contain the bulk of your robot commands
     """
     global turn_count
+    global is_messanger
     turn_count += 1
 
     if get_type() == UnitType.SOLDIER:
         run_soldier()
     elif get_type() == UnitType.MOPPER:
+        if get_id() % 2 == 0: is_messanger = True
         run_mopper()
     elif get_type() == UnitType.SPLASHER:
         run_splasher()
@@ -71,6 +83,8 @@ def turn():
 
 def run_tower():
     global buildCooldown
+    global savingTurns
+    global should_save
     # Pick a direction to build in.
     dir = directions[random.randint(0, len(directions) - 1)]
     loc = get_location()
@@ -89,18 +103,30 @@ def run_tower():
 
     # Pick a random robot type to build.
     # Should hold off on building since we're gonna end up with all moppers!
-    if buildCooldown <= 0: 
-        robot_type = get_random_unit(bot_chance)
-        if can_build_robot(robot_type, next_loc):
-            build_robot(robot_type, next_loc)
-            buildCooldown = buildDelay + random.randint(-buildDeviation, buildDeviation)
-            log("BUILT A " + bot_name[robot_type])
-    else: buildCooldown -= 1
-        
+    if savingTurns <= 0:
+        should_save = False
+        if buildCooldown <= 0: 
+            robot_type = get_random_unit(bot_chance)
+            if can_build_robot(robot_type, next_loc):
+                build_robot(robot_type, next_loc)
+                buildCooldown = buildDelay + random.randint(-buildDeviation, buildDeviation)
+                log("BUILT A " + bot_name[robot_type])
+
+    if buildCooldown > 0: buildCooldown -= 1
+    if savingTurns > 0: 
+        savingTurns -= 1
+        log("Saving for " + savingTurns + " more turns")
+
     # Read incoming messages
     messages = read_messages()
     for m in messages:
         log(f"Tower received message: '#{m.get_sender_id()}: {m.get_bytes()}'")
+
+        # If we are not currently saving and we receive the save chips message, start saving
+        if not should_save and m.get_bytes() == int(MessageType.SAVE_CHIPS):
+            savingTurns = save_turns
+            should_save = True
+
 
 
 def run_soldier():
@@ -110,10 +136,14 @@ def run_soldier():
     # Search for a nearby ruin to complete.
     cur_ruin = None
     tower_type = None
-    if cur_ruin == None:
-        for tile in nearby_tiles:
-            if tile.has_ruin():
+    cur_dist = 999999
+    for tile in nearby_tiles:
+        if tile.has_ruin() and sense_robot_at_location(tile.get_map_location()) == None:
+            check_dist = tile.get_map_location().distance_squared_to(get_location())
+            if check_dist < cur_dist:
+                cur_dist = check_dist
                 cur_ruin = tile
+
 
     if cur_ruin is not None:
         target_loc = cur_ruin.get_map_location()
@@ -121,7 +151,7 @@ def run_soldier():
         if can_move(dir):
             move(dir)
 
-        if tower_type == None: tower_type = get_random_unit(tower_chance)
+        tower_type = get_random_unit(tower_chance)
         # Mark the pattern we need to draw to build a tower here if we haven't already.
         should_mark = cur_ruin.get_map_location().subtract(dir)
         if sense_map_info(should_mark).get_mark() == PaintType.EMPTY and can_mark_tower_pattern(tower_type, target_loc):
@@ -157,6 +187,16 @@ def run_soldier():
 
 
 def run_mopper():
+    if is_messanger:
+        set_indicator_dot(get_location(), 255, 0, 0)
+
+    if should_save and len(known_towers) > 0:
+        # Move to first known tower if we are saving
+        dir = get_location().direction_to(known_towers[0])
+        set_indicator_string(f"Returning to {known_towers[0]}")
+        if can_move(dir):
+            move(dir)
+    
     # Move and attack randomly.
     dir = directions[random.randint(0, len(directions) - 1)]
     next_loc = get_location().add(dir)
@@ -185,6 +225,9 @@ def run_mopper():
                 mop_loc = get_location().add(mop_dir)
                 if can_attack(mop_loc): attack(mop_loc)
                 break
+
+    if is_messanger:
+        update_friendly_towers()
 
     # We can also move our code into different methods or classes to better organize it!
     update_enemy_robots()
@@ -218,6 +261,52 @@ def run_splasher():
             else:
                 attack(loc, False)
 
+def check_nearby_ruins():
+    global should_save
+    nearby_tiles = sense_nearby_map_infos(center=get_location())
+
+    # Search for a nearby ruin to complete.
+    cur_ruin = None
+    for tile in nearby_tiles:
+        tile_loc = tile.get_map_location()
+        if not tile.has_ruin() or sense_robot_at_location(tile_loc) != None:
+            continue
+        
+        # Heuristic to see if the ruin is trying to be built on
+        mark_loc = tile_loc.add(tile_loc.direction_to(get_location()))
+        mark_info = sense_map_info(mark_loc)
+        if not mark_info.get_mark().is_ally():
+            continue
+
+        should_save = True
+
+        # Return early
+        return
+
+    
+def update_friendly_towers():
+    global should_save
+
+    # Search for all nearby robots
+    ally_robots  = sense_nearby_robots(team=get_team())
+    for ally in ally_robots:
+        # Only consider tower type
+        if not ally.get_type().is_tower_type():
+            continue
+
+        ally_loc = ally.location
+        if ally_loc in known_towers:
+            # Send a message to the nearby tower
+            if should_save and can_send_message(ally_loc):
+                send_message(ally_loc, int(MessageType.SAVE_CHIPS))
+                should_save = False
+
+            # Skip adding to the known towers array
+            continue
+
+        # Add to our known towers array
+        known_towers.append(ally_loc)
+        set_indicator_string(f"Found tower {ally.get_id()}")
 
 def update_enemy_robots():
     # Sensing methods can be passed in a radius of -1 to automatically 
@@ -235,7 +324,7 @@ def update_enemy_robots():
 
     # Occasionally try to tell nearby allies how many enemy robots we see.
     ally_robots = sense_nearby_robots(team=get_team())
-    if get_round_num() % 20 == 0:
-        for ally in ally_robots:
-            if can_send_message(ally.location):
-                send_message(ally.location, len(enemy_robots))
+    # if get_round_num() % 20 == 0:
+    #     for ally in ally_robots:
+    #         if can_send_message(ally.location):
+    #             send_message(ally.location, len(enemy_robots))
